f71c0a2350d95438e9b6ed9667c3c083ceb0c2c2
Camille <lecamille@gmail.com>
2024-04-16 16:01:32 -0700

    Try again

---
+++ tutorials/11-fs-async.js
@@ -0,0 +1,31 @@
+// Asynchronous - provide a callback
+// We run that callback when we are done (when whatever functionality
+// we have done is complete, such as with addEventListener)
+
+const { readFile, writeFile } = require('fs');
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return;
+  }
+  const first = result
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const second = result
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log(result)
+      }
+    )
+  })
+})
