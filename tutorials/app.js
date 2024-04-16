41c4e2d6c68fe11de0a0caf3821147c2ee998795
Camille <lecamille@gmail.com>
2024-04-16 14:38:40 -0700

    Add fs tutorials

--- tutorials/app.js
+++ tutorials/app.js
@@ -1,11 +1,11 @@
-const path = require('path')
-console.log(path.sep)
+const { readFileSync, writeFileSync } = require('fs')
 
-const filePath = path.join('/content','subfolder','test.txt')
-console.log(filePath)
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
 
-const base = path.basename(filePath)
-console.log(base)
+console.log(first, second)
 
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
-console.log(absolute)
+writeFileSync(
+  './content/result-sync.txt',
+  `Here is the result : ${first}, ${second}`
+)
