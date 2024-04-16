957cb011bb775d725f22fc85e00c217f87ff0422
Camille <lecamille@gmail.com>
2024-04-16 14:55:44 -0700

    Update

--- tutorials/app.js
+++ tutorials/app.js
@@ -1,11 +1,31 @@
-const { readFileSync, writeFileSync } = require('fs')
+// Asynchronous - provide a callback
+// We run that callback when we are done (when whatever functionality
+// we have done is complete, such as with addEventListener)
 
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const { readFile, writeFile } = require('fs');
 
-console.log(first, second)
-
-writeFileSync(
-  './content/result-sync.txt',
-  `Here is the result : ${first}, ${second}`
-)
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return;
+  }
+  const first = result
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const second = result
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log(result)
+      }
+    )
+  })
+})
