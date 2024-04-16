73643a0305509f8a1e42e29d04d1388bc8de0a89
Camille <lecamille@gmail.com>
2024-04-16 16:49:29 -0700

    Update

--- tutorials/app.js
+++ tutorials/app.js
@@ -1,18 +1,8 @@
 // http module
 
-const http = require('http')
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Welcome to our home page')
-  } else if (req.url == '/about') {
-    res.end('Here is our short history')
-  } else {
-    res.end(`
-      <h1>Oops!</h1>
-      <p>We can't seem to find the page you are looking for</p>
-      <a href="/">back home</a>
-    `)
-  }
-})
+const _ = require('lodash')
 
-server.listen(5000)
+const items = [1,[2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+
+console.log(newItems)
