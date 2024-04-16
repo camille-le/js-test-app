646a4d0164f7757bc52f02d04ea2b29af02cacb1
Camille <lecamille@gmail.com>
2024-04-16 14:25:58 -0700

    Update JS

--- tutorials/app.js
+++ tutorials/app.js
@@ -1,10 +1,15 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
 
-const names = require('./4-names')
-const sayHi = require('./5-utils')
+const os = require('os')
+const user = os.userInfo()
 
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem()
+}
 
+console.log(currentOS)
