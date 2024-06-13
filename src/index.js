7243e157e678274010916c05c837bb2b9f9cf4fe
Camille <lecamille@gmail.com>
2024-06-12 23:05:23 -0700

    Update index js

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 
 const port = 3000;
 app.listen(port, () => {
-  console.log('Server running on http://localhost:${port}');
+  console.log(`Server running on http://localhost:${port}`);
 });
